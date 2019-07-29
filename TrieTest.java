31dfdcaaed2e4e286e2959e10732afc93da46cf8
gleeyan <25153775+gleeyan@users.noreply.github.com>
2019-07-29 09:37:16 -0500

    Add files via upload

---
+++ TrieTest.java
@@ -0,0 +1,19 @@
+package com.codingdojo.tries;
+
+public class TrieTest {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insertWord("car");
+        trie.insertWord("card");
+        trie.insertWord("chip");
+        trie.insertWord("trie");
+        trie.insertWord("try");
+        System.out.println("All the keys in the Trie:");
+        trie.printAllKeys();
+        System.out.println(trie.isPrefixValid("a"));
+        System.out.println(trie.isPrefixValid("b"));
+        System.out.println(trie.isWordValid("car"));
+        System.out.println(trie.isWordValid("dog"));
+        
+    }
+}
